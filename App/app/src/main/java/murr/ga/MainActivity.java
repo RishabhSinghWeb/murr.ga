68f696bc1f13ee8ed45251183d8a00bd690b93ea
Rishabh Singh <jokingcross@gmail.com>
2020-12-08 11:47:07 +0530

    Update MainActivity.java

--- App/app/src/main/java/murr/ga/MainActivity.java
+++ App/app/src/main/java/murr/ga/MainActivity.java
@@ -7,32 +7,29 @@ import android.content.*;
 import android.webkit.*;
 public class MainActivity extends Activity 
 {
-	private WebView wv1;
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-		
-		wv1=findViewById(R.id.webview);
-		wv1.setWebViewClient(new MyWebViewClient());
-		wv1.getSettings().setJavaScriptEnabled(true);
-		wv1.getSettings().setSupportZoom(true);
-		wv1.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
-		wv1.loadUrl("murr.ga");
-				
-    }
-	
+	private WebView wv;
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		wv = findViewById(R.id.webview);
+		wv.setWebViewClient(new MyWebViewClient());
+		wv.getSettings().setJavaScriptEnabled(true);
+		wv.getSettings().setSupportZoom(true);
+		wv.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+		wv.loadUrl("murr.ga");
+	}
 
 	public final class MyWebViewClient extends WebViewClient {
-
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            Intent i = new Intent(Intent.ACTION_VIEW, request.getUrl());
-            startActivity(i);
-            return true;
-        }
-
-    }
-
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+			Intent intent = new Intent(Intent.ACTION_VIEW, request.getUrl());
+			startActivity(intent);
+			return true;
+		}
+		
+	}
+	
 }
